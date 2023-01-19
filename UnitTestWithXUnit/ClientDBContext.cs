2f296d35c627b44b448ea4fd9fd4718fffa1e46b
Sandip Ghimire <sandip.ghimire@omnicommediagroup.com>
2023-01-19 12:37:39 +0545

    Added Model, Make Connectionstring to fetch data

---
+++ UnitTestWithXUnit/ClientDBContext.cs
@@ -0,0 +1,21 @@
+﻿using DomainModel.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitTestWithXUnit
+{
+    public class ClientDBContext : DbContext
+    {
+        #region DbSet
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+        public ClientDBContext(DbContextOptions<ClientDBContext> options) : base(options)
+        {
+
+        }
+        #endregion
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        }
+
+    }
+}
